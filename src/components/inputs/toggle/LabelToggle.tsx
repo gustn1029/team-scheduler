e0e2f6a0a9a6c57adf9dc4a6f9bb85bd8baee180
hs0820 <gustn1029@gmail.com>
2024-09-25 16:38:58 +0900

    토글 컴포넌트 생성

---
+++ src/components/inputs/toggle/LabelToggle.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { LabelInputProps } from "../../../types";
+import style from "./labelToggle.module.scss";
+
+type ToggleProps = Pick<
+  LabelInputProps,
+  "errorView" | "error" | "register" | "label" | "watch"
+>;
+
+const LabelToggle = (props: ToggleProps) => {
+  return (
+    <label htmlFor={props.label} className={`${style.toggle}`}>
+      <input
+        type="checkbox"
+        {...props.register}
+        id={props.label}
+        checked={props.watch(props.label) === true}
+      />
+      <span></span>
+    </label>
+  );
+};
+
+export default LabelToggle;
