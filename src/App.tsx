37b4fefcb80aeb8d8ea73d13535f330b6a4b6cf1
hs0820 <gustn1029@gmail.com>
2024-09-25 10:58:05 +0900

    리액트 쿼리 provider 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./utils/http";
+import Login from "./components/pages/Login";
+import { useUserData } from "./hooks/useUserDataHook";
 
 function App() {
+  useUserData();
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <h1>team-scheduler</h1>
-    </QueryClientProvider>
+      <Login />
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./utils/http";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <App />
+    </QueryClientProvider>
   </StrictMode>
 );
