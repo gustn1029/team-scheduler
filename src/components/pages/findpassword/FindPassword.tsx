ffb61a43850b55e70c48f5cbd5cb870dd8d79945
LeeMinseo0117 <leems3164@naver.com>
2024-10-02 14:32:13 +0900

    비밀번호 찾기 화면 구현

---
+++ src/components/pages/findpassword/FindPassword.tsx
@@ -0,0 +1,10 @@
+import styles from "./findpassword.module.scss";
+function FindPassword() {
+  return (
+    <>
+      <h1 className={styles.h1}>준비중...</h1>
+    </>
+  );
+}
+
+export default FindPassword;
