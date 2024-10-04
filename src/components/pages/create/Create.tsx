5dc93d0eae33a7bde0bcd0bc5ece2df98827fff2
biktory01 <biktory36@gmail.com>
2024-10-04 17:41:58 +0900

    캘린더 추가, timepicker 적용중, 메모 적용중

--- src/components/pages/create/Create.tsx
+++ src/components/pages/create/Create.tsx
@@ -3,7 +3,10 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { RiCloseFill } from 'react-icons/ri';
 import { IoMdCheckmark } from 'react-icons/io';
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import styles from './create.module.scss';
+import { ko } from 'date-fns/locale/ko';
 import LabelInput from '../../inputs/input/LabelInput';
 
 interface FormInputs {
@@ -57,9 +60,64 @@ const Create: React.FC = () => {
   };
 
   const [isChecked, setIsChecked] = useState(false);
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+
+  const initalTime = new Date();
+  initalTime.setHours(12, 0, 0, 0);
+
+  const [startTime, setStartTime] = useState<Date | null>(initalTime);
+  const [endTime, setEndTime] = useState<Date | null>(initalTime);
 
   const handleToggleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(event.target.checked);
+  };
+
+  const handleStartDateChange = (date: Date | null) => {
+    if (date) {
+      setStartDate(date);
+      if (!endDate || date > endDate) {
+        setEndDate(date);
+      }
+    }
+  };
+
+  const handleEndDateChange = (date: Date | null) => {
+    if (date) {
+      if (startDate && date < startDate) {
+        setStartDate(date);
+      }
+      setEndDate(date);
+    }
+  };
+
+  const CustomTimePicker: React.FC<{ selectedTime: Date | null, onTimeChange: React.Dispatch<React.SetStateAction<Date | null>> }> = ({ selectedTime, onTimeChange }) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const time = event.target.value;
+      const [hours, minutes] = time.split(':').map(Number);
+
+      const updatedDate = new Date();
+      updatedDate.setHours(hours, minutes, 0, 0);
+
+      onTimeChange(updatedDate);
+    };
+
+    const getTimeValue = (date: Date | null) => {
+      if (!date) return '';
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+      return `${hours}:${minutes}`;
+    };
+
+    return (
+      <input
+        className={styles.timeInput}
+        type="time"
+        value={getTimeValue(selectedTime)}
+        onChange={handleChange}
+        step="900"
+      />
+    );
   };
 
   return (
@@ -73,7 +131,7 @@ const Create: React.FC = () => {
           </button>
         </header>
         <div className={styles.sheduleWriter}>
-          <span>작성자</span>
+          <h3>작성자</h3>
           <img
             className={styles.writerProfile}
             src=""
@@ -87,6 +145,7 @@ const Create: React.FC = () => {
             placeholder="제목"
             register={register("title", {
               required: { value: true, message: "제목을 입력하세요." },
+              minLength: { value: 3, message: "제목은 최소 3자 이상 입력하세요." },
             })}
             watch={watch}
             ariaInvalid={
@@ -99,7 +158,7 @@ const Create: React.FC = () => {
         </div>
         <div className={`${styles.colorSelect} ${isOpen ? 'on' : ''}`}>
           <div className={styles.colorFlex}>
-            <span className={styles.colorTitle}>색상 선택</span>
+            <h3 className={styles.colorTitle}>색상 선택</h3>
             <button className={styles.val} type="button" onClick={toggleSelectBox}>
               <div className={`${styles.colorBox} ${styles[selectedColor]}`}></div>
               <span className={styles.selectName}>{selectedText}</span>
@@ -117,14 +176,58 @@ const Create: React.FC = () => {
             </ul>
           )}
         </div>
-        <div className={styles.calendarArea}>
+        <div className={styles.toggleArea}>
           <label className={styles.toggleStyle} htmlFor="toggleSwitch" role="switch">
-            <span>하루 종일</span>
-            <input id="toggleSwitch" type="checkbox" />
+            <h3>하루 종일</h3>
+            <input
+              id="toggleSwitch"
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleToggleChange}
+            />
           </label>
         </div>
-        <div>
-          
+        <div className={styles.calendar}>
+          <div className={styles.pickerGroup}>
+            <DatePicker
+              className={styles.datePicker}
+              selected={startDate}
+              onChange={handleStartDateChange}
+              dateFormat='yyyy년 MM월 dd일 (EEE)'
+              locale={ko}
+              placeholderText="시작 날짜를 선택하세요"
+            />
+            {!isChecked && (
+              <div className={styles.timePickerWrapper}>
+                <CustomTimePicker selectedTime={startTime} onTimeChange={setStartTime} />
+              </div>
+            )}
+          </div>
+          <div className={styles.pickerGroup}>
+            <DatePicker
+              className={styles.datePicker}
+              selected={endDate}
+              onChange={handleEndDateChange}
+              dateFormat='yyyy년 MM월 dd일 (EEE)'
+              locale={ko}
+              placeholderText="종료 날짜를 선택하세요"
+            />
+            {!isChecked && (
+              <div className={styles.timePickerWrapper}>
+                <CustomTimePicker
+                  selectedTime={endTime}
+                  onTimeChange={setEndTime}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className={styles.meMo}>
+          <h3>메모</h3>
+          <textarea
+            className={styles.textArea}
+            placeholder="메모를 입력하세요">
+          </textarea>
         </div>
       </form>
     </main>
