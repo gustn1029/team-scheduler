056f3fa5b8dd307384a3ddc9006f8b480d592661
hs0820 <gustn1029@gmail.com>
2024-10-08 14:48:26 +0900

    RootLayout 추가 로그인 정보 없으면 로그인 화면으로 이동되도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Home from "./components/pages/home/Home";
 import Login from "./components/pages/login/Login";
-import { useUserData } from "./hooks/useUserDataHook";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./components/pages/Profile";
 import Create from "./components/pages/create/Create";
@@ -10,23 +9,25 @@ import NotFound from "./components/pages/notFound/NotFound";
 import Loader from "./components/loader/Loader";
 import ToastProvider from "./components/toast/ToastProvider";
 import CalendarList from "./components/pages/calendarlist/CalendarList";
+import RootLayout from "./components/layouts/RootLayout";
 
 function App() {
-  useUserData();
   return (
     <>
       <BrowserRouter>
         <Loader />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/findpassword" element={<FindPassword />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/calendarlist" element={<CalendarList />} />
-        </Routes>
+        <RootLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/findpassword" element={<FindPassword />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/calendarlist" element={<CalendarList />} />
+          </Routes>
+        </RootLayout>
         <ToastProvider />
       </BrowserRouter>
     </>
