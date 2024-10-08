4f7c266e45e6a2958fc53965f0df8bcc67b19c56
biktory01 <biktory36@gmail.com>
2024-10-08 14:44:54 +0900

    사용자 profile 불러오기 완료, datepicker, timepicker 작업 진행중

--- src/components/pages/create/Create.tsx
+++ src/components/pages/create/Create.tsx
@@ -1,62 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { RiCloseFill } from 'react-icons/ri';
 import { IoMdCheckmark } from 'react-icons/io';
-import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from './create.module.scss';
 import { ko } from 'date-fns/locale/ko';
 import LabelInput from '../../inputs/input/LabelInput';
+import CustomTimePicker from './CustomTimePicker';
+import { useQuery } from '@tanstack/react-query';
+import { userDataFetch } from '../../../utils/http';
+import { appAuth } from '../../../firebase/config';
 
 interface FormInputs {
   title: string;
 }
 
 const Create: React.FC = () => {
+  const { data: authData } = useQuery({
+    queryKey: ["auth", appAuth.currentUser?.uid],
+    queryFn: () => userDataFetch(appAuth.currentUser?.uid as string),
+    enabled: !!appAuth.currentUser?.uid,
+  });
+
+  console.log(authData);
+
   const {
     handleSubmit,
     register,
     watch,
     formState: { errors, isSubmitted, isSubmitting },
-    reset,
   } = useForm<FormInputs>();
 
   const onSubmit: SubmitHandler<FormInputs> = (data) => {
     console.log(data);
-  };
-
-  const handleClose = () => {
-    console.log('close');
-  };
-
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedColor, setSelectedColor] = useState('blue');
-  const [selectedText, setSelectedText] = useState('Blue');
-
-  const colorOptions = [
-    { colorClass: 'red', colorName: 'Red' },
-    { colorClass: 'pink', colorName: 'Pink' },
-    { colorClass: 'orange', colorName: 'Orange' },
-    { colorClass: 'yellow', colorName: 'Yellow' },
-    { colorClass: 'mint', colorName: 'Mint' },
-    { colorClass: 'blue', colorName: 'Blue' },
-    { colorClass: 'gray', colorName: 'Gray' },
-  ];
-
-  useEffect(() => {
-    setSelectedColor('blue');
-    setSelectedText('Blue');
-  }, []);
-
-  const handleColorSelect = (colorClass: string, colorName: string) => {
-    setSelectedColor(colorClass);
-    setSelectedText(colorName);
-    setIsOpen(false);
-  };
-
-  const toggleSelectBox = () => {
-    setIsOpen(!isOpen);
   };
 
   const [isChecked, setIsChecked] = useState(false);
@@ -69,56 +46,42 @@ const Create: React.FC = () => {
   const [startTime, setStartTime] = useState<Date | null>(initalTime);
   const [endTime, setEndTime] = useState<Date | null>(initalTime);
 
-  const handleToggleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(event.target.checked);
+  const [openComponent, setOpenComponent] = useState<string | null>(null);
+
+  // 시간 동기화 함수
+  // const handleTimeSync = (start: Date | null, end: Date | null) => {
+  //   if (start && end && start > end) {
+  //     setEndTime(start); // 시작 시간이 종료 시간보다 이후인 경우, 종료 시간을 시작 시간으로 맞춤
+  //   }
+  // };
+
+  const handleStartTimeChange: React.Dispatch<React.SetStateAction<Date | null>> = (time) => {
+    setStartTime(time);
+  };
+  
+  const handleEndTimeChange: React.Dispatch<React.SetStateAction<Date | null>> = (time) => {
+    setEndTime(time);
   };
 
   const handleStartDateChange = (date: Date | null) => {
-    if (date) {
-      setStartDate(date);
-      if (!endDate || date > endDate) {
-        setEndDate(date);
-      }
-    }
-  };
-
-  const handleEndDateChange = (date: Date | null) => {
-    if (date) {
-      if (startDate && date < startDate) {
-        setStartDate(date);
-      }
+    setStartDate(date);
+    if (date && (!endDate || date > endDate)) {
       setEndDate(date);
     }
   };
 
-  const CustomTimePicker: React.FC<{ selectedTime: Date | null, onTimeChange: React.Dispatch<React.SetStateAction<Date | null>> }> = ({ selectedTime, onTimeChange }) => {
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const time = event.target.value;
-      const [hours, minutes] = time.split(':').map(Number);
-
-      const updatedDate = new Date();
-      updatedDate.setHours(hours, minutes, 0, 0);
-
-      onTimeChange(updatedDate);
-    };
-
-    const getTimeValue = (date: Date | null) => {
-      if (!date) return '';
-      const hours = date.getHours().toString().padStart(2, '0');
-      const minutes = date.getMinutes().toString().padStart(2, '0');
-      return `${hours}:${minutes}`;
-    };
-
-    return (
-      <input
-        className={styles.timeInput}
-        type="time"
-        value={getTimeValue(selectedTime)}
-        onChange={handleChange}
-        step="900"
-      />
-    );
+  const handleEndDateChange = (date: Date | null) => {
+    setEndDate(date);
+    if (date && startDate && date < startDate) {
+      setStartDate(date);
+    }
   };
+
+  const handleToggleComponent = (component: string) => {
+    setOpenComponent(openComponent === component ? null : component);
+  };
+
+  const profileImage = authData?.imageUrl || 'default-profile-image-url';
 
   return (
     <main>
@@ -134,7 +97,7 @@ const Create: React.FC = () => {
           <h3>작성자</h3>
           <img
             className={styles.writerProfile}
-            src=""
+            src={profileImage}
             alt="writerProfile"
           />
         </div>
@@ -143,39 +106,18 @@ const Create: React.FC = () => {
             type="text"
             label="Title"
             placeholder="제목"
-            register={register("title", {
-              required: { value: true, message: "제목을 입력하세요." },
-              minLength: { value: 3, message: "제목은 최소 3자 이상 입력하세요." },
+            register={register('title', {
+              required: { value: true, message: '제목을 입력하세요.' },
+              minLength: { value: 3, message: '제목은 최소 3자 이상 입력하세요.' },
             })}
             watch={watch}
-            ariaInvalid={
-              isSubmitted ? (errors.title ? true : false) : undefined
-            }
+            ariaInvalid={isSubmitted ? (errors.title ? true : false) : undefined}
             error={errors}
             errorView={errors.title}
             isLabelTextHidden={true}
           />
         </div>
-        <div className={`${styles.colorSelect} ${isOpen ? 'on' : ''}`}>
-          <div className={styles.colorFlex}>
-            <h3 className={styles.colorTitle}>색상 선택</h3>
-            <button className={styles.val} type="button" onClick={toggleSelectBox}>
-              <div className={`${styles.colorBox} ${styles[selectedColor]}`}></div>
-              <span className={styles.selectName}>{selectedText}</span>
-              <i className={styles.selectIcon}>{isOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}</i>
-            </button>
-          </div>
-          {isOpen && (
-            <ul className={`${styles.colorList} ${isOpen ? styles.view : styles.hidden}`}>
-              {colorOptions.map((option) => (
-                <li className={styles.selectOption} key={option.colorClass} onClick={() => handleColorSelect(option.colorClass, option.colorName)}>
-                  <div className={`${styles.colorBox} ${styles[option.colorClass]}`}></div>
-                  <span className={styles.listName}>{option.colorName}</span>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+
         <div className={styles.toggleArea}>
           <label className={styles.toggleStyle} htmlFor="toggleSwitch" role="switch">
             <h3>하루 종일</h3>
@@ -183,51 +125,60 @@ const Create: React.FC = () => {
               id="toggleSwitch"
               type="checkbox"
               checked={isChecked}
-              onChange={handleToggleChange}
+              onChange={(e) => setIsChecked(e.target.checked)}
             />
           </label>
         </div>
+
         <div className={styles.calendar}>
           <div className={styles.pickerGroup}>
             <DatePicker
               className={styles.datePicker}
               selected={startDate}
               onChange={handleStartDateChange}
-              dateFormat='yyyy년 MM월 dd일 (EEE)'
+              dateFormat="yyyy년 MM월 dd일 (EEE)"
               locale={ko}
               placeholderText="시작 날짜를 선택하세요"
+              open={openComponent === 'startDate'}
+              onInputClick={() => handleToggleComponent('startDate')}
+              onClickOutside={() => setOpenComponent(null)}
             />
             {!isChecked && (
-              <div className={styles.timePickerWrapper}>
-                <CustomTimePicker selectedTime={startTime} onTimeChange={setStartTime} />
-              </div>
+              <CustomTimePicker
+                selectedTime={startTime}
+                onTimeChange={handleStartTimeChange}
+                isOpen={openComponent === 'startTime'}
+                onToggle={() => handleToggleComponent('startTime')}
+              />
             )}
           </div>
+
           <div className={styles.pickerGroup}>
             <DatePicker
               className={styles.datePicker}
               selected={endDate}
               onChange={handleEndDateChange}
-              dateFormat='yyyy년 MM월 dd일 (EEE)'
+              dateFormat="yyyy년 MM월 dd일 (EEE)"
               locale={ko}
               placeholderText="종료 날짜를 선택하세요"
+              open={openComponent === 'endDate'}
+              onInputClick={() => handleToggleComponent('endDate')}
+              onClickOutside={() => setOpenComponent(null)}
             />
             {!isChecked && (
-              <div className={styles.timePickerWrapper}>
-                <CustomTimePicker
-                  selectedTime={endTime}
-                  onTimeChange={setEndTime}
-                />
-              </div>
+              <CustomTimePicker
+                selectedTime={endTime}
+                onTimeChange={handleEndTimeChange}
+                isOpen={openComponent === 'endTime'}
+                onToggle={() => handleToggleComponent('endTime')}
+              />
             )}
           </div>
         </div>
+
         <div className={styles.meMo}>
           <h3>메모</h3>
-          <textarea
-            className={styles.textArea}
-            placeholder="메모를 입력하세요">
-          </textarea>
+          <textarea className={styles.textArea} placeholder="메모를 입력하세요"></textarea>
         </div>
       </form>
     </main>
