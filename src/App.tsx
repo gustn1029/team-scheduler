ada262a86f94906723de349c3bf8c362957664bf
hs0820 <gustn1029@gmail.com>
2024-10-08 17:21:28 +0900

    todo 관련 라우트, home 라우트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Home from "./components/pages/home/Home";
 import Login from "./components/pages/login/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Profile from "./components/pages/Profile";
 import Create from "./components/pages/create/Create";
 import { SignUp } from "./components/pages/signup/SignUp";
@@ -10,8 +10,13 @@ import Loader from "./components/loader/Loader";
 import ToastProvider from "./components/toast/ToastProvider";
 import CalendarList from "./components/pages/calendarlist/CalendarList";
 import RootLayout from "./components/layouts/RootLayout";
+import TodosCreate from "./components/pages/todo/create/TodosCreate";
+import TodoList from "./components/pages/todo/list/TodoList";
+import TodosEdit from "./components/pages/todo/edit/TodosEdit";
+
 
 function App() {
+
   return (
     <>
       <BrowserRouter>
@@ -19,6 +24,7 @@ function App() {
         <RootLayout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/create" element={<Create />} />
             <Route path="/login" element={<Login />} />
@@ -26,6 +32,9 @@ function App() {
             <Route path="/findpassword" element={<FindPassword />} />
             <Route path="*" element={<NotFound />} />
             <Route path="/calendarlist" element={<CalendarList />} />
+            <Route path="/todo-create" element={<TodosCreate />} />
+            <Route path="/todo-list" element={<TodoList />} />
+            <Route path="/todo-edit" element={<TodosEdit />} />
           </Routes>
         </RootLayout>
         <ToastProvider />
