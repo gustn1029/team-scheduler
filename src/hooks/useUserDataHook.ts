c39a04bc9a8ee85bb5ac3ce1e095791b1137d396
hs0820 <gustn1029@gmail.com>
2024-10-08 17:21:13 +0900

    로그인 정보 관련 훅 수정

--- src/hooks/useUserDataHook.ts
+++ src/hooks/useUserDataHook.ts
@@ -1,23 +1,24 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { appAuth } from "../firebase/config";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
 
 export const useUserData = () => {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(appAuth, (user) => {
-      if (user) {
-        queryClient.invalidateQueries({ queryKey: ["auth", appAuth.currentUser?.uid] });
-      } else {
-        navigate("/login");
+    const unsubscribe = onAuthStateChanged(appAuth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        queryClient.invalidateQueries({
+          queryKey: ["auth", currentUser.uid],
+        });
       }
     });
 
     return () => unsubscribe();
-  }, [queryClient, navigate]);
+  }, [queryClient]);
+
+  return { user };
 };
