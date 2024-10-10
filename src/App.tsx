fb786fc23b2100d11efb52e2b9e2699e37a1627c
hs0820 <gustn1029@gmail.com>
2024-10-10 16:47:14 +0900

    로그인 상태 관리하던 것 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import CalendarComponent from "./components/pages/calendar/Calendar";
-import Login from "./components/pages/login/Login";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/pages/login/Login";
+import CalendarComponent from "./components/pages/calendar/CalendarComponent";
 import Profile from "./components/pages/Profile";
 import Create from "./components/pages/create/Create";
 import { SignUp } from "./components/pages/signup/SignUp";
@@ -9,7 +9,6 @@ import NotFound from "./components/pages/notFound/NotFound";
 import Loader from "./components/loader/Loader";
 import ToastProvider from "./components/toast/ToastProvider";
 import CalendarList from "./components/pages/calendarlist/CalendarList";
-import RootLayout from "./components/layouts/RootLayout";
 import Todos from "./components/pages/todos/Todos";
 
 function App() {
@@ -17,20 +16,18 @@ function App() {
     <>
       <BrowserRouter>
         <Loader />
-        <RootLayout>
-          <Routes>
-            <Route path="/" element={<Navigate to={"/calendar"} replace />} />
-            <Route path="/calendar" element={<CalendarComponent />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/findpassword" element={<FindPassword />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/calendarlist" element={<CalendarList />} />
-            <Route path="/todo" element={<Todos />} />
-          </Routes>
-        </RootLayout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/calendar" replace />} />
+          <Route path="/calendar" element={<CalendarComponent />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/findpassword" element={<FindPassword />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/calendarlist" element={<CalendarList />} />
+          <Route path="/todo" element={<Todos />} />
+        </Routes>
         <ToastProvider />
       </BrowserRouter>
     </>
