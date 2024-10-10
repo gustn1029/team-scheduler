bce19519c3f2b8d60475b211ee7108c56667b091
hs0820 <gustn1029@gmail.com>
2024-10-10 09:44:54 +0900

    유저 정보 체크하는 방법 수정

--- src/hooks/useUserDataHook.ts
+++ src/hooks/useUserDataHook.ts
@@ -1,24 +1,31 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { appAuth } from "../firebase/config";
-import { useEffect, useState } from "react";
-import { onAuthStateChanged, User } from "firebase/auth";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const useUserData = () => {
   const queryClient = useQueryClient();
-  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
+    const userData = sessionStorage.getItem("user");
     const unsubscribe = onAuthStateChanged(appAuth, (currentUser) => {
-      setUser(currentUser);
+      
       if (currentUser) {
         queryClient.invalidateQueries({
           queryKey: ["auth", currentUser.uid],
         });
       }
+      
+      const userSession = {
+        email: currentUser?.email,
+        displayName: currentUser?.displayName
+      }
+
+      if(userData === null) {
+        sessionStorage.setItem("user", JSON.stringify(userSession));
+      }
     });
 
     return () => unsubscribe();
   }, [queryClient]);
-
-  return { user };
 };
