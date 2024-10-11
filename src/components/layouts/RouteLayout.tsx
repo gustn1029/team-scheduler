71fde97292bdaaf145d85a19220e1321a6036fe3
hs0820 <gustn1029@gmail.com>
2024-10-11 18:11:06 +0900

    로그인 정보 확인해서 라우팅 되도록 확인

---
+++ src/components/layouts/RouteLayout.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import { useAuthState } from "../../hooks/useAuthState";
+import Loader from "../loader/Loader";
+
+const RouteLayout = () => {
+  const isAuthenticated = sessionStorage.getItem("user");
+  const navigate = useNavigate();
+  const { user, loading } = useAuthState();
+
+  useEffect(() => {
+    if (!isAuthenticated && !user) {
+      console.log(isAuthenticated);
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate, user]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (isAuthenticated && user) ? <Outlet /> : null;
+};
+
+export default RouteLayout;
