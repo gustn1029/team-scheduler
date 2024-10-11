aedce64a012f58fa34cb67468d8a230eb626e387
hs0820 <gustn1029@gmail.com>
2024-10-11 18:11:22 +0900

    모달 컴포넌트 수정 scss 추가

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,30 +1,42 @@
 import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
+import styles from  './modal.module.scss';
 
 interface ModalProps {
   children: React.ReactNode;
+  isOpen: boolean;
   onClose: () => void;
 }
 
-export default function Modal({ children, onClose }: ModalProps) {
+export default function Modal({ children, isOpen, onClose }: ModalProps) {
   const dialog = useRef<HTMLDialogElement | null>(null);
 
   useEffect(() => {
     const modal = dialog.current;
     if (modal) {
-      modal.showModal();
-    }
-
-    return () => {
-      if (modal) {
+      if (isOpen) {
+        modal.showModal();
+      } else {
         modal.close();
       }
-    };
-  }, []);
+    }
+  }, [isOpen]);
+
+  const handleBackgroundClick = (e: React.MouseEvent<HTMLDialogElement>) => {
+    if (e.target === dialog.current) {
+      onClose();
+    }
+  };
 
   return createPortal(
-    <dialog className="modal" ref={dialog} onClose={onClose}>
-      {children}
+    <dialog 
+      className={styles.modal} 
+      ref={dialog} 
+      onClick={handleBackgroundClick}
+    >
+      <div onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
     </dialog>,
     document.getElementById('modal') as HTMLElement
   );
