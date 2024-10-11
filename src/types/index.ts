37c666e68c9a36a2c5596c86fc19732f0705dd83
hs0820 <gustn1029@gmail.com>
2024-10-11 18:13:53 +0900

    투두 관련 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -43,6 +43,11 @@ export interface LabelSelectOptionsProps extends LabelInputProps {
   optionList: OptionList[];
 }
 
+export interface DeleteFetchProps {
+  collectionName: string;
+  uid: string;
+}
+
 // 이벤트 엔티티
 export interface EventsData {
   id?: string;
@@ -70,9 +75,15 @@ export interface Holiday {
   seq: number;
 }
 
+export interface HolidayDataFetchProps {
+  year: number;
+  month: number;
+}
+
 export interface EventsFetchProps {
   year: number;
   month: number;
+  uid: string;
 }
 
 export type EventPostData = Omit<
@@ -80,17 +91,45 @@ export type EventPostData = Omit<
   "category" | "todos" | "comments" | "like" | "createDate"
 >;
 
+export interface EventsFetchProps {
+  year: number;
+  month: number;
+  uid: string;
+}
+
+export interface CalendarTodos {
+  id: string;
+  todoDate: Date
+}
+
 export interface TodoData {
-  id?: string;
-  todo?: string;
-  isComplete?: boolean;
+  id?:string;
+  todos: TodoItem[];
   todoDate?: Date;
   createDate?: Date;
   updateDate?: Date | null;
+  uid?: string;
 }
 
 export interface TodoItem {
   id?: string;
   isComplete?: boolean;
   todo?: string;
+  createDate?: Date;
+}
+
+export interface GetTodosFetchProps {
+  date: Date;
+  uid: string;
+}
+
+export interface TodoAddFetchProps {
+  data: TodoData;
+  date: Date;
+  uid: string;
+}
+
+export interface TodoUpdateFetchProps{
+  data: TodoItem[];
+  uid: string;
 }
