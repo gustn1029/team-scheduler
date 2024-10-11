e54d396016f4dd8bdfec51bf2a2158b11ee70259
hs0820 <gustn1029@gmail.com>
2024-10-11 18:13:31 +0900

    투두 스타일링 수정 및 기능 추가

--- src/components/pages/todos/TodoList.tsx
+++ src/components/pages/todos/TodoList.tsx
@@ -1,21 +1,43 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./todos.module.scss";
 import { useTodoStore } from "../../../store/useTodoStore";
-import { TodoData } from "../../../types";
+import { TodoItem } from "../../../types";
 import TodoListItem from "./TodoListItem";
+import { useQuery } from "@tanstack/react-query";
+import { getTodosFetch } from "../../../utils/http";
+import { appAuth } from "../../../firebase/config";
 
 const TodoList = () => {
-  const { todos } = useTodoStore();
-  const [notCompleteTodos, setNotCompleteTodos] = useState<TodoData[]>([]);
-  const [completeTodos, setCompleteTodos] = useState<TodoData[]>([]);
-  console.log(todos);
+  const { selectedDate, todos, setTodos } = useTodoStore();
+  const [notCompleteTodos, setNotCompleteTodos] = useState<TodoItem[]>([]);
+  const [completeTodos, setCompleteTodos] = useState<TodoItem[]>([]);
 
-  useMemo(() => {
-    const notCompelte = todos.filter((el) => el.isComplete === false);
+  const {
+    data: todoData,
+  } = useQuery({
+    queryKey: [
+      "todos",
+      appAuth.currentUser?.uid,
+      selectedDate.toISOString().split("T")[0],
+    ],
+    queryFn: () =>
+      getTodosFetch({ date: selectedDate, uid: appAuth.currentUser!.uid }),
+  });
+
+  useEffect(() => {
+    if (todoData && todoData.length > 0) {
+      setTodos(todoData[0]?.todos);
+    } else {
+      setTodos([]);
+    }
+  }, [todoData, setTodos]);
+
+  useEffect(() => {
+    const notComplete = todos.filter((el) => el?.isComplete === false);
     const complete = todos.filter((el) => el.isComplete === true);
 
-    setNotCompleteTodos(notCompelte);
+    setNotCompleteTodos(notComplete);
     setCompleteTodos(complete);
   }, [todos]);
 
