8df6f904f0b5ec7b1bd20daab5a82e7f1746753c
hs0820 <gustn1029@gmail.com>
2024-10-11 18:10:11 +0900

    라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import Login from "./components/pages/login/Login";
 import CalendarComponent from "./components/pages/calendar/CalendarComponent";
 import Profile from "./components/pages/Profile";
@@ -10,12 +10,62 @@ import Loader from "./components/loader/Loader";
 import ToastProvider from "./components/toast/ToastProvider";
 import CalendarList from "./components/pages/calendarlist/CalendarList";
 import Todos from "./components/pages/todos/Todos";
+import RouteLayout from "./components/layouts/RouteLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RouteLayout />,
+    children: [
+      { index: true, element: <Navigate to="/calendar" replace /> },
+      {
+        path: "calendar",
+        element: <CalendarComponent />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "create",
+        element: <Create />,
+      },
+      {
+        path: "calendarlist",
+        element: <CalendarList />,
+      },
+      {
+        path: "todo",
+        element: <Todos />,
+      },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/findpassword",
+    element: <FindPassword />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Loader />
+      <Loader />
+      <RouterProvider router={router} />
+      <ToastProvider />
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/calendar" replace />} />
           <Route path="/calendar" element={<CalendarComponent />} />
@@ -28,8 +78,7 @@ function App() {
           <Route path="/calendarlist" element={<CalendarList />} />
           <Route path="/todo" element={<Todos />} />
         </Routes>
-        <ToastProvider />
-      </BrowserRouter>
+        </BrowserRouter> */}
     </>
   );
 }
