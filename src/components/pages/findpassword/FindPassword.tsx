bb9154aea95d34d5ddf377918270ead51b8f3758
hs0820 <gustn1029@gmail.com>
2024-10-20 17:42:48 +0900

    비밀번호 찾기, 404 페이지  애니메이션 추가

--- src/components/pages/findpassword/FindPassword.tsx
+++ src/components/pages/findpassword/FindPassword.tsx
@@ -2,10 +2,12 @@ import styles from "./findpassword.module.scss";
 import LinkButton from "../../button/LinkButton";
 import { ButtonStyleEnum } from "../../../types/enum/ButtonEnum";
 import preparing from "../../../assets/images/preparing.png";
+import MainAnimationLayout from "../../layouts/MainAnimationLayout";
+import { layoutXVarients } from "../../../utils/Animations";
 
 function FindPassword() {
   return (
-    <main className={styles.main}>
+    <MainAnimationLayout variants={layoutXVarients} className={styles.main}>
       <h1 className={styles.h1}>
         <img src={preparing} alt="준비중 이미지" />
         준비중...
@@ -13,7 +15,7 @@ function FindPassword() {
       <LinkButton href="/login" buttonStyle={ButtonStyleEnum.Normal}>
         로그인
       </LinkButton>
-    </main>
+    </MainAnimationLayout>
   );
 }
 
