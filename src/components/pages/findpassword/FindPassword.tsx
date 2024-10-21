aa770aa01635275a8de09b2074fdc588a445c46b
hs0820 <gustn1029@gmail.com>
2024-10-21 10:14:59 +0900

    처음 진입 시 끊기는 것 같은 현상 수정

--- src/components/pages/findpassword/FindPassword.tsx
+++ src/components/pages/findpassword/FindPassword.tsx
@@ -3,15 +3,22 @@ import LinkButton from "../../button/LinkButton";
 import { ButtonStyleEnum } from "../../../types/enum/ButtonEnum";
 import preparing from "../../../assets/images/preparing.png";
 import MainAnimationLayout from "../../layouts/MainAnimationLayout";
-import { layoutXVarients } from "../../../utils/Animations";
+import { layoutXVarients, opacityVarients } from "../../../utils/Animations";
+
+import { motion } from "framer-motion";
 
 function FindPassword() {
   return (
     <MainAnimationLayout variants={layoutXVarients} className={styles.main}>
-      <h1 className={styles.h1}>
+      <motion.h1
+        variants={opacityVarients}
+        initial="hidden"
+        animate="visible"
+        className={styles.h1}
+      >
         <img src={preparing} alt="준비중 이미지" />
         준비중...
-      </h1>
+      </motion.h1>
       <LinkButton href="/login" buttonStyle={ButtonStyleEnum.Normal}>
         로그인
       </LinkButton>
